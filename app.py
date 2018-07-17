114adbb79b2a179511a376e3521857cb45e03f68
Rahul Shrivastava <rahul3103@gmail.com>
2018-07-17 14:08:52 +0530

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, jsonify, request, render_template
 import json
+import base64
 
 app = Flask(__name__)
 
@@ -15,8 +16,11 @@ def view():
 @app.route('/_ah/push-handlers/receive_message', methods=['POST'])
 def listings():
     if request.method == 'POST':
-        listing = json.loads(request.data)
-        a.append(listing)
+        req_data = json.loads(request.data)['message']
+        coded_string = req_data['data']
+        history_id = eval(base64.b64decode(coded_string))['historyId']
+        req_data['computed'] = {'history_id': history_id}
+        a.append({req_data['publishTime']: req_data})
         return jsonify({'success': True}), 200
 
 
